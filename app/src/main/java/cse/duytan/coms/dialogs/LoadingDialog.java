f6ba480683091338ea5277a55c9d04ea3f44a085
Pham Van Thien <pvthiendeveloper@gmail.cpom>
2017-06-15 14:39:49 -0700

    Thien

---
+++ app/src/main/java/cse/duytan/coms/dialogs/LoadingDialog.java
@@ -0,0 +1,10 @@
+package cse.duytan.coms.dialogs;
+
+/**
+ * Created by Pham Van Thien on 6/15/2017.
+ * Email: pvthiendeveloper@gmail.com
+ * Phone: 0979477093
+ */
+
+public class LoadingDialog {
+}
---
+++ app/src/main/java/cse/duytan/coms/presenters/LoginPresenter.java
@@ -0,0 +1,10 @@
+package cse.duytan.coms.presenters;
+
+/**
+ * Created by Pham Van Thien on 6/15/2017.
+ * Email: pvthiendeveloper@gmail.com
+ * Phone: 0979477093
+ */
+
+public interface LoginPresenter {
+}
