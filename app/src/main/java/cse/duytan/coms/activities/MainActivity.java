c043c730c9151e41fc5074b6d48c09c694a0b537
lehoangdung <lehoangdung9x@gmail.com>
2017-06-15 15:14:09 +0700

    command

--- app/src/main/java/cse/duytan/coms/activities/MainActivity.java
+++ app/src/main/java/cse/duytan/coms/activities/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dũng 15-6-2017
+        //dũng 15/6/2017
     }
 }
