857ec8cc464fd6158661023efe2e2c8702dfb078
Pham Van Thien <pvthiendeveloper@gmail.cpom>
2017-06-15 14:07:47 -0700

    Init

---
+++ app/src/main/java/cse/duytan/coms/untils/Constants.java
@@ -0,0 +1,10 @@
+package cse.duytan.coms.untils;
+
+/**
+ * Created by Pham Van Thien on 6/15/2017.
+ * Email: pvthiendeveloper@gmail.com
+ * Phone: 0979477093
+ */
+/*Giao diện dùng để chứa các biến dùng chung cho dự án*/
+public interface Constants {
+}
---
+++ app/src/main/java/cse/duytan/coms/untils/Utils.java
@@ -0,0 +1,11 @@
+package cse.duytan.coms.untils;
+
+/**
+ * Created by Pham Van Thien on 6/15/2017.
+ * Email: pvthiendeveloper@gmail.com
+ * Phone: 0979477093
+ */
+
+/*Lớp dùng để chứa các hàm dùng chung cho dự án*/
+public class Utils {
+}
