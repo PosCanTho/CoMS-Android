24c35e9f200690d7202f3b5c0e7a2fc44dde816e
Pham Van Thien <pvthiendeveloper@gmail.cpom>
2017-07-10 10:05:20 +0700

    Đã ghép giao diện của Kiệt.

--- app/src/main/java/cse/duytan/coms/activities/BaseActivity.java
+++ app/src/main/java/cse/duytan/coms/activities/BaseActivity.java
@@ -1,5 +1,6 @@
 package cse.duytan.coms.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,11 +8,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import cse.duytan.coms.connections.DownloadCallback;
 import cse.duytan.coms.untils.AdapterCallback;
 import cse.duytan.coms.untils.Constants;
 import cse.duytan.coms.untils.PopupCalback;
 
-public class BaseActivity extends AppCompatActivity implements Constants, View.OnClickListener, PopupCalback, AdapterCallback, ListView.OnItemClickListener {
+public class BaseActivity extends AppCompatActivity implements Constants, View.OnClickListener, PopupCalback, AdapterCallback, ListView.OnItemClickListener,
+        DownloadCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,11 @@ public class BaseActivity extends AppCompatActivity implements Constants, View.O
         }
     }
 
+    public void showHomeButton(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
     @Override
     public void popupCalback(int processId, Object data) {
 
@@ -47,6 +55,16 @@ public class BaseActivity extends AppCompatActivity implements Constants, View.O
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void downloadSuccess(int processId, Object data) {
+
+    }
+
+    @Override
+    public void downloadError(int processId, String msg) {
 
     }
 }
