29512092f8f66ff23c787dbf94e26757d7dcb103
Pham Van Thien <pvthiendeveloper@gmail.cpom>
2017-07-10 10:05:43 +0700

    Đã ghép giao diện của Kiệt.

---
+++ app/src/main/java/cse/duytan/coms/activities/ConferenceInfomationActivity.java
@@ -0,0 +1,43 @@
+package cse.duytan.coms.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+
+import cse.duytan.coms.R;
+import cse.duytan.coms.adapters.ListConferenceSessionAdapter;
+
+public class ConferenceInfomationActivity extends AppCompatActivity {
+    private ListView ls;
+    private ListConferenceSessionAdapter adapter;
+    private ArrayList<String> listData = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conference_infomation);
+        setUp();
+    }
+
+    private void setUp() {
+        ls = (ListView) findViewById(R.id.lsConferenceSession);
+        listData.add("a");
+        listData.add("b");
+        listData.add("c");
+        adapter = new ListConferenceSessionAdapter(getApplicationContext(), listData);
+        ls.setAdapter(adapter);
+        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ConferenceInfomationActivity.this, ConferenceSessionInfomationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
