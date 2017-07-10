6c72a4ed768f60015d01c9f1a85f51c93edece42
Pham Van Thien <pvthiendeveloper@gmail.cpom>
2017-07-10 10:19:08 +0700

    Đã ghép giao diện của Kiệt.

---
+++ app/src/main/java/cse/duytan/coms/presenters/MessagePresenter.java
@@ -0,0 +1,12 @@
+package cse.duytan.coms.presenters;
+
+/**
+ * Created by Pham Van Thien on 7/7/2017.
+ * Email: pvthiendeveloper@gmail.com
+ * Phone: 0979477093
+ */
+
+public class MessagePresenter {
+    public MessagePresenter() {
+    }
+}
